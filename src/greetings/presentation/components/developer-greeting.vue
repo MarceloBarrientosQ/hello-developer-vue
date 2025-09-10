45109faef3ce6ae8c697a1b3ee1eb13bbb728dbb
Marcelo Barrientos Q <u20221e646@upc.edu.pe>
2025-09-10 10:42:03 -0500

    fix(greetings):  add developer-greeting component

--- src/greetings/presentation/components/developer-greeting.vue
+++ src/greetings/presentation/components/developer-greeting.vue
@@ -1,9 +1,23 @@
 <script setup lang="js">
 
+import {computed} from "vue";
+import {Developer} from "../../domain/model/developer.entity.js";
+
+const props = defineProps(['firstName', 'lastName']);
+
+const greeting = computed(() => {
+  if (!props.firstName?.trim() || !props.lastName?.trim()) {
+    return 'Welcome Anonymus Developer';
+  }
+  const developer = new Developer(props.firstName, props.lastName);
+  return `Congrats ${developer.fullName}! Now you are a registered developer.`;
+});
+
+
 </script>
 
 <template>
-
+  <p>{{ greeting }}</p>
 </template>
 
 <style scoped>
