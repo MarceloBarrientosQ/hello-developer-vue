cd484e1bd407903afa30a9b5d6a0c2d545e0f4be
Marcelo Barrientos Q <u20221e646@upc.edu.pe>
2025-09-10 10:56:01 -0500

    fix(greetings):  add developer components to app component

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,52 @@
 <script setup>
+import {ref} from "vue";
+import {Developer} from "./greetings/domain/model/developer.entity.js";
+import DeveloperRegistrarion from "./greetings/presentation/components/developer-registrarion.vue";
+import DeveloperCountShow from "./greetings/presentation/components/developer-count-show.vue";
+import DeveloperGreeting from "./greetings/presentation/components/developer-greeting.vue";
 
+const firstName = ref('');
+const lastName = ref('');
+const developerCount = ref(0);
+const hasRegistered = ref(false);
+
+function updateRegisteredDeveloperInfo(developer) {
+  firstName.value = developer.firstName;
+  lastName.value = developer.lastName;
+  hasRegistered.value = true;
+  console.log('Developer registered', developer);
+  updateDeveloperCount(developer);
+}
+
+function resetRegisteredDeveloperInfo() {
+  firstName.value = '';
+  lastName.value = '';
+  hasRegistered.value = false;
+  console.log('Developer registration reset');
+}
+
+function updateDeveloperCount(developer) {
+  const dev = new Developer(developer.firstName, developer.lastName);
+
+  if(dev.fullName !== 'Unknown'){
+    developerCount.value++;
+  }
+
+  console.log('Developer count updated', developerCount.value);
+}
 </script>
 
 <template>
+  <h1>Hello Vue Developer App</h1>
 
+  <developer-registrarion @developer-registered="updateRegisteredDeveloperInfo"
+                          @registration-deferred="resetRegisteredDeveloperInfo" />
+
+  <developer-greeting v-if="hasRegistered"
+                      :first-name="firstName"
+                      :last-name="lastName" />
+
+  <developer-count-show :developer-count="developerCount" />
 </template>
 
 <style scoped>
